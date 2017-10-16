e2188e84f576cbf53594608833a0f75f713a54da
Pedro Henrique Carvalho <pedrohenrique@nex2me.com.br>
2017-10-16 14:19:39 -0300

    DEVELOP Documenting a bit better

--- data_processing.py
+++ data_processing.py
@@ -12,13 +12,16 @@ def generate_dataset(coeffs, mean=0, var=5, no_of_timesteps=30):
 
 	Returns a nd.array containing an artificial dataset, i.e. a set of measurements for the original signal
 	'''
-
+	# Here we create the array of transformations as specified in transformation.py
 	transformations = [power(i) for i in range(len(coeffs))]
 
+	# A function that transforms an integer x into an array [1	x] and then multiplies it by the coefficients of the model
 	original_signal = lambda x: np.dot(transform(x, transformations), coeffs)
-	
-	noises = np.random.normal(mean, var, no_of_timesteps)
 
+	# Here we sample a gaussian N(mean, var) to get the error values for each time step
+	noises = [np.random.normal(mean, var) for _ in range(no_of_timesteps)]
+
+	# We sum the original signal and the noise to get the corrupted dataset
 	return np.array([np.array([i, original_signal(i) + noises[i]]) for i in range(no_of_timesteps)])
 
 def plot_data(data, name):
